06a8f9cd74549c91ed8ceba9fac08b68556985ea
marcusps11 <marcus@1roof.com>
2015-10-30 15:46:51 +0000

    first time using sass hopefully this is not too painful

--- js/app.js
+++ js/app.js
@@ -1,2 +1,16 @@
 angular
-  .module('flickrApp', ['ngResource'])
+.module('flickrApp', ['ngResource', 'ui.router'])
+.config(MainRouter);
+
+MainRouter.$inject = ['$stateProvider', '$urlRouterProvider']; 
+
+function MainRouter($stateProvider, $urlRouterProvider) { 
+  $stateProvider
+  .state('home',
+    { url: "/", 
+    templateUrl: "home.html"
+  })
+
+  $urlRouterProvider.otherwise('/');
+}
+
--- js/flickrController.js
+++ js/flickrController.js
@@ -7,38 +7,29 @@ FlickrController.$inject = ['$http']
 
 function FlickrController($http){
   var self = this 
-  this.newPhoto = {}
+  self.photo = null
   self.all = []
   self.images = []
 
-
-
   this.getPhoto = function(){
-    console.log('gettinghere')
+    console.log('am')
     $http
     .jsonp('https://api.flickr.com/services/feeds/photos_public.gne?tags=potato&tagmode=all&format=json&jsoncallback=JSON_CALLBACK')
     .then(function(response){
       var data = response.data.items
       angular.forEach(data, function(data, key){
-      console.log(key, data)
-      self.images.push(data.media)
-      self.all.push(data)
+        self.images.push(data.media)
+        self.all.push(data)
+      })
 
-
-    })
-
-
-    })
+    })  
   }
 
- //  self.addPhoto = function(data){
- //    angular.forEach(data, function(data) {
- //      self.all.push(data);
- //    })
- //    // console.log(self.all)
+  self.showPhoto = function(photo){
+    self.photo = photo.flickr.media.m
+    console.log(self.photo)
+   }
 
- // }
+  
+ }
 
-
-
-}
