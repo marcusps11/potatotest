555e5dda8729b13e76c636db8ce8d542a5c8482b
marcusps11 <marcus@1roof.com>
2015-11-02 15:09:39 +0000

    sass gruntfile added

--- js/flickrController.js
+++ js/flickrController.js
@@ -8,8 +8,27 @@ FlickrController.$inject = ['$http']
 function FlickrController($http){
   var self = this 
   self.photo = null
+  self.tags = null
   self.all = []
   self.images = []
+  self.term = null
+
+self.search = function(term){
+  console.log(term)
+  self.term = term
+
+
+  var d = new Date(term)
+  var n = d.getDate();
+  var year = d.getFullYear()
+  var month = d.getMonth()
+  console.log(n)
+  console.log(year)
+  console.log(month)
+  console.log(monthString)
+
+
+}
 
   this.getPhoto = function(){
     console.log('am')
@@ -19,6 +38,7 @@ function FlickrController($http){
       var data = response.data.items
       angular.forEach(data, function(data, key){
         self.images.push(data.media)
+        console.log(data)
         self.all.push(data)
       })
 
@@ -27,6 +47,7 @@ function FlickrController($http){
 
   self.showPhoto = function(photo){
     self.photo = photo.flickr.media.m
+    self.tags = photo.flickr.tags
     console.log(self.photo)
    }
 
